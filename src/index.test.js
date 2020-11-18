1a2f5ab8eab4d06720263f235fba6ca62cb643ba
HoracioAbalos <horacio.mathias@gmail.com>
2020-11-18 16:42:54 -0300

    refactor: 0.1.6
    Modifica archivos aceptados

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,25 @@
+const { classExpression } = require("@babel/types");
+const { test, expect } = require("@jest/globals");
 const { timeStamp } = require("console");
 const { hasUncaughtExceptionCaptureCallback } = require("process");
 const functions = require("./index");
 
-test("Validación nombre", () => {
+test("Validación string ", () => {
   expect(functions.validarString("")).toBe(false);
 });
 
-test("Validación nombre", () => {
+test("Validación string", () => {
   expect(functions.validarString(null)).toBe(false);
 });
+
+test("Validación string", () => {
+  expect(functions.validarString("Horacio")).toBe(true);
+});
+
+/*
+test("Validacion de archivo tablatura", () => {
+  expect(functions.validateFileTab("")).toBe(false);
+});
+*/
+
+/*VALIDAR ARCHIVO CANCION*/
